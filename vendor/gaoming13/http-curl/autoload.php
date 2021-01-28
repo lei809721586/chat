062bc2980f6a94c570e1fa0a19d352f66f3d20fc
leiwenfeng <lei809721586@qq.com>
2021-01-28 16:21:36 +0800

    初始化api

--- vendor/gaoming13/http-curl/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-spl_autoload_register(function ($class) {
-    var_dump($class);
-    if (false !== stripos($class, 'Gaoming13\HttpCurl')) {
-        require_once __DIR__.'/'.str_replace('\\', DIRECTORY_SEPARATOR, substr($class, 18)).'.php';
-    }
-});
